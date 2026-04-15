f96c0477407e5c4fc4a4fc3c1b9388b4398f9117
GM2140358\ydauser01 <ryuuha10.28@ezweb.ne.jp>
2026-04-15 14:55:24 +0900

    シーンマネージャーを追加

---
+++ Scene.cpp
@@ -0,0 +1,19 @@
+#include"Scene.h"
+void Title::Draw() {
+
+}
+void Title::Update() {
+
+}
+void Game::Draw() {
+
+}
+void Game::Update() {
+
+}
+void Result::Draw() {
+
+}
+void Result::Update() {
+
+}
